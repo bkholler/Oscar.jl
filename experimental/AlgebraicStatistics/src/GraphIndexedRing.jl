10d72abc140b16f5e7214189124206334112a6ec
antonydellavecchia <antonydellavecchia@gmail.com>
2025-02-28 16:03:57 +0100

    progress on graph indexed ring

--- experimental/AlgebraicStatistics/src/GraphIndexedRing.jl
+++ experimental/AlgebraicStatistics/src/GraphIndexedRing.jl
@@ -1,11 +1,24 @@
-struct GraphIndexedRing{T, EdgeMap{T, S}, NodeMap{T, U}, V}
+struct GraphIndexedRing{T, EdgeMap{T, S}, NodeMap{T, U}, V} where W <: Union{MPolyRingElem{V}, Vector{MPolyRingElem{V}}}
   ring::MPolyRing{V}
   g::Graph{T}
-  edge_gens::Dict{S, MPolyRingElem{V}}
-  node_gens::Dict{U, MPolyRingElem{V}}
+  edge_gens::Dict{S, W}
+  node_gens::Dict{U, W}
 end
 
-function graph_indexed_ring(F::Field, G::Graph{T}, edge_var="e", node_var="v")
+function graph_indexed_ring(F::Field, G::Graph{T};
+                            edge_var::VarName="e", node_var::VarName="v",
+                            edge_ngens::Int=1, node_ngens::Int=1,
+                            cached=false)
   edge_map = get_attribute!(G, :edge_map, nothing)
+  node_map = get_attribute!(G, :node_map, nothing)
+  if !isnothing(edge_map)
+    e_indices = unique(e -> G[e], edges(G))
+  else
+    
+  end
   
+  R, e_gens, v_gens = polynomial_ring(
+    F,
+
+  )
 end
